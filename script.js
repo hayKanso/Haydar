95925ec6a6acfb99ef219c8bce0ed68b47944df5
hayKanso <kansohay@gmail.com>
2021-04-13 10:54:19 +0200

    version 1.0

---
+++ script.js
@@ -0,0 +1,50 @@
+/*var maVar ="ma valeur";
+(function(){
+    var maVar = "Valeur locale";
+    console.log(maVar);
+})();
+/*var maVar ="ma valeur";
+(function(){
+    var maVar = "Valeur locale";
+    console.log(maVar);
+})();
+*/
+
+/*Var num1 =1;
+var num2 = 2;
+var res;
+(fonction(){
+    res = num1/num2;
+    return res;
+    console.log(res)
+}
+
+)
+*/
+
+/*function reverseTab(tab) {
+    let res = [];
+    for (let i = tab.length -1; i > -1; i--) {
+      res.push(tab[i]);
+    }
+    
+    return res;
+  }
+  console.log(reverseTab([1, 2, 3])); 
+ console.log(reverseTab([1, 1, 2, 2, 3])); 
+*/
+
+ /*function getLastElem(tab) { 
+    return tab[tab.length - 1] 
+  } 
+  // Afficher la sortie 
+  console.log(getLastElem([1, 2, 3, 4])); 
+  console.log(getLastElem(["Alex", "Bob", "Emily"])); 
+  */
+
+  tab = [1,2,3,4]
+  for(i=0; i< tab.length; i ++){
+  console.log(tab[i]);
+  }
+  
+  
